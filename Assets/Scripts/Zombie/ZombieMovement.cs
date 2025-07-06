a85d934d6adcf53e9d1ebec44ee7adb0a47e09a7
Muhammad Hasham <m.hasham1239@gmail.com>
2025-07-06 19:12:21 +0500

    feat (Zombie) : Zombie Movement and Navmesh implemented

---
+++ Assets/Scripts/Zombie/ZombieMovement.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class ZombieMovement : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private Transform target;
+
+    [Header("Detection Settings")]
+    [SerializeField] private float detectionRadius = 10f;
+    [SerializeField] private LayerMask playerLayer;
+
+    [Header("Attack Settings")]
+    [SerializeField] private float attackRange = 2f;
+
+    private void Reset()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Awake()
+    {
+        if (agent == null)
+            agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        DetectPlayer();
+        HandleMovement();
+    }
+
+    private void DetectPlayer()
+    {
+        Collider[] hits = Physics.OverlapSphere(transform.position, detectionRadius, playerLayer);
+        if (hits.Length > 0)
+        {
+            target = hits[0].transform;
+        }
+        else
+        {
+            target = null;
+        }
+    }
+
+    private void HandleMovement()
+    {
+        if (target != null)
+        {
+            float distance = Vector3.Distance(transform.position, target.position);
+            if (distance > attackRange)
+            {
+                agent.isStopped = false;
+                agent.SetDestination(target.position);
+            }
+            else
+            {
+                agent.isStopped = true;
+                // Attack logic will go here later
+            }
+        }
+        else
+        {
+            agent.ResetPath();
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, detectionRadius);
+    }
+}
